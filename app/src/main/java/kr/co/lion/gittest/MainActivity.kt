863b912db1480b00a1579a826c86b1a5892c35d3
jay <emilesscheater@gmail.com>
2024-02-08 13:33:06 +0900

    test commit 6

--- app/src/main/java/kr/co/lion/gittest/MainActivity.kt
+++ app/src/main/java/kr/co/lion/gittest/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-    var a1=10
-
+        var a1=10
     }
 }
